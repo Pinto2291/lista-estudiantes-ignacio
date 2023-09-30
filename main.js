58eed907b36ce063d5620c81dfecbabc9eba422f
pinto22 <51886210+Pinto2291@users.noreply.github.com>
2023-09-29 21:32:10 -0400

    jquery agregado y css modificado, las listas ahora son desplegables

---
+++ main.js
@@ -0,0 +1,7 @@
+$('#list-1').on('click', () => {
+    $(event.currentTarget).siblings().toggle('slow');
+})
+
+$('#list-2').on('click', () => {
+    $(event.currentTarget).siblings().toggle('slow');
+})
